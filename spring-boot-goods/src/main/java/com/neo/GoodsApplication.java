1c99fda315f195527374e657e838216970cf8634
peter <513316565@qq.coom>
2025-11-04 23:04:59 +0800

    CN.4100559659083088:7cbe313bcc390bd85697a221db6127c6_690a07b27e3b2c81398adf8a.690a0b907e3b2c81398adf8e.690a0b8f40cb5ec88b7a1f64:Trae CN.T(2025/11/4 22:20:00)

--- spring-boot-goods/src/main/java/com/neo/GoodsApplication.java
+++ spring-boot-goods/src/main/java/com/neo/GoodsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoodsApplication {
-
+    // 启动类
 	public static void main(String[] args) {
 		SpringApplication.run(GoodsApplication.class, args);
 	}
